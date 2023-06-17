71359f86b5d8b2671f341f0f54c591d49db22e29
tonylim <dohyun0047@gmail.com>
2023-06-17 18:19:20 +0900

    fixed adder to behave in ascending order indexing might look confusing but it is more natural when we print out the matrix

--- adder.c
+++ adder.c
@@ -16,18 +16,55 @@ int main(void)
         size_t z = x + y;
         size_t overflow = z >= n;
         for (size_t j = 0; j < BITS; ++j) {
-            MAT_AT(ti, i, j)        = (x>>j)&1;
-            MAT_AT(ti, i, j + BITS) = (y>>j)&1;
-            if (overflow) {
-                MAT_AT(ti, i, j)        = 0;
-                MAT_AT(ti, i, j + BITS) = 0;
-                MAT_AT(to, i, j)        = 0;
-            } else {
-                MAT_AT(to, i, j)        = (z>>j)&1;
-            }
+            MAT_AT(ti, i, BITS - 1 -j)        = (x>>j)&1;
+            MAT_AT(ti, i, ti.cols - 1 - j) = (y>>j)&1;
+            MAT_AT(to, i, BITS - 1 - j)        = (z>>j)&1;
         }
+        MAT_AT(to, i, BITS) = overflow;
     }
+
     MAT_PRINT(ti);
     MAT_PRINT(to);
+
+
+    size_t arch[] = {2*BITS, 2 * BITS, BITS + 1};
+    NN nn = nn_alloc(arch, ARRAY_LEN(arch));
+    NN g = nn_alloc(arch, ARRAY_LEN(arch));
+    nn_rand(nn, 0, 1);
+    // NN_PRINT(nn);
+
+    float rate = 1;
+
+    // printf("c = %f\n", nn_cost(nn, ti, to));
+    for (size_t i = 0; i < 10 *1000; ++i) {
+        nn_backprop(nn, g, ti, to);
+        nn_learn(nn, g, rate);
+        // printf("%zu: c = %f\n", i, nn_cost(nn, ti, to));
+    }
+
+    for (size_t x = 0; x < n; ++x) {
+        for (size_t y = 0; y < n; ++y) {
+            printf("%zu + %zu = ", x, y);
+            for (size_t j = 0; j < BITS; ++j) {
+                MAT_AT(NN_INPUT(nn), 0, BITS -1 -j)    = (x>>j)&1;
+                MAT_AT(NN_INPUT(nn), 0, ti.cols -1 -j) = (y>>j)&1;
+            }
+            nn_forward(nn);
+            if (MAT_AT(NN_OUTPUT(nn), 0, BITS) > 0.5f) {
+                printf("OVERFLOW\n");
+            } else {
+                size_t z = 0;
+                for (size_t j = 0; j < BITS; ++j) {
+                    size_t bit = MAT_AT(NN_OUTPUT(nn), 0, j) > 0.5f;
+                    z |= bit << j;
+                }
+                printf("%zu\n", z);
+            }
+
+        }
+    }
+
+
+
 
 }
