5c2e564a6a6c6b0fa9fe2353d21db1524caf47e8
kuuku123 <dohyun0047@gmail.com>
2023-12-31 15:08:25 +0900

    test layout to manage easily

--- testQT/build.sh
+++ testQT/build.sh
@@ -6,7 +6,4 @@ LIBS="`pkg-config --libs raylib` -lm -lglfw -ldl -lpthread"
 NN_H="-I /home/tony/workspace/ml/c/Tsoding_MachineLearning_in_C/framework/"
 STB_IMAGE="-I /home/tony/workspace/ml/c/Tsoding_MachineLearning_in_C/software/raylib/src/external/"
 
-clang $CFLAGS $NN_H -o adder_gen adder_gen.c $LIBS
-clang $CFLAGS $NN_H -o xor_gen xor_gen.c $LIBS
-clang $CFLAGS $STB_IMAGE $NN_H -o img2mat img2mat.c $LIBS
-
+clang $CFLAGS -o layout layout.c $LIBS
---
+++ testQT/layout.c
@@ -0,0 +1,86 @@
+#include <assert.h>
+#include <stdio.h>
+#include <raylib.h>
+
+typedef struct {
+    float x;
+    float y;
+    float w;
+    float h;
+} Layout_Rect;
+
+void widget(Layout_Rect r, Color c) 
+{
+    DrawRectangle(r.x, r.y, r.w, r.h, c);
+    printf("widget(%f, %f,%f,%f)\n", r.x, r.y, r.w, r.h);
+}
+
+typedef enum {
+    LO_HORZ,
+    LO_VERT,
+} Layout_Orient;
+
+typedef struct {
+    Layout_Orient orient;
+    Layout_Rect rect;
+    size_t i;
+    size_t count;
+} Layout;
+
+Layout_Rect layout_slot(Layout *l) 
+{
+    assert(l-> i < l-> count);
+    Layout_Rect r = {0};
+
+
+    switch (l -> orient) {
+        case LO_HORZ:
+            r.w = l-> rect.w / l-> count;
+            r.h = l-> rect.h;
+            r.x = l-> rect.x + l->i * r.w;
+            r.y = l-> rect.y;
+        break;
+        
+        case LO_VERT:
+            r.w = l-> rect.w;
+            r.h = l-> rect.h / l-> count;
+            r.x = l-> rect.x;
+            r.y = l-> rect.y + l->i * r.h;
+        break;
+
+        default:
+            assert(0 && "Unreachable");
+    }
+    l-> i += 1;
+    return r;
+}
+
+int main(void) 
+{
+    size_t factor = 80;
+    size_t width = 16 * factor;
+    size_t height = 9 * factor;
+
+    InitWindow(width, height, "Layout");
+    SetTargetFPS(60);
+
+
+    while (!WindowShouldClose()) {
+        Layout root = {
+            .orient = LO_HORZ,
+            .rect = {0, 0, width, height},
+            .count = 3,
+        };
+
+        BeginDrawing();
+            ClearBackground(BLACK);
+            widget(layout_slot(&root),GREEN);
+            widget(layout_slot(&root),BLUE);
+            widget(layout_slot(&root),RED);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
